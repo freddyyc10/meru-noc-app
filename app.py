5280f351723145c3daa6f9289ddac3750ccd05e0
freddyyc10 <freddy.yaguaramay@gmail.com>
2026-04-02 01:31:34 -0400

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, 
   AreaChart, Area 
